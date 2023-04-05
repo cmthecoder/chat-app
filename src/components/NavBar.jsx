8fe3a9524ee75de2ef30cc63cdd907b0f0988297
Chris Mems <christopher.mems@.com>
2023-04-05 09:37:41 -0500

    style navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+const style = {
+  nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
+  heading: `text-white text-3xl`
+}
+
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className={style.nav}>
+      <h1 className={style.heading}>Chat App</h1>
+    </div>
   )
 }
 
